5d287a88001b7b913a4be4903b9760f5bca303c4
Camilo2816 <e.sanchezn@uniandes.edu.co>
2026-03-03 09:02:50 -0800

    Initial project setup
    author

---
+++ GN1-Swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GN1-Swift
+//
+//  Created by Camilo Sanchez Novoa on 2/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GN1-Swift/GN1_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  GN1_SwiftApp.swift
+//  GN1-Swift
+//
+//  Created by Camilo Sanchez Novoa on 2/03/26.
+//
+
+import SwiftUI
+
+@main
+struct GN1_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
